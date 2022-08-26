47a205ae3defd89e65b84a8c79cff27145313371
Saima Akter <74361691+Saimatonni@users.noreply.github.com>
2022-08-26 22:56:12 +0600

    Update leaf_node.cpp

--- search_and_tree/leaf_node.cpp
+++ search_and_tree/leaf_node.cpp
@@ -1,18 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
-/* A binary tree node has data,
-pointer to left child and
-a pointer to right child */
+
 struct node
 {
     int data;
     struct node* left;
     struct node* right;
 };
- 
-/* Function to get the count
-of leaf nodes in a binary tree*/
+
 unsigned int getLeafCount(struct node* node)
 {
     if(node == NULL)    
@@ -24,8 +19,6 @@ unsigned int getLeafCount(struct node* node)
             getLeafCount(node->right);
 }
  
-/* Helper function that allocates a new node with the
-given data and NULL left and right pointers. */
 struct node* newNode(int data)
 {
     struct node* node = (struct node*)
@@ -47,10 +40,8 @@ int main()
     root->left->left = newNode(4);
     root->left->right = newNode(5);
      
-/*get leaf count of the above created tree*/
 cout << "Leaf count of the tree is : "<<
                 getLeafCount(root) << endl;
 return 0;
 }
  
-// This code is contributed by SHUBHAMSINGH10
