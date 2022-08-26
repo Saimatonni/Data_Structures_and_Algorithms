652c3748a8d3e8c35da0262ffe6880f86adf104b
Saima Akter <74361691+Saimatonni@users.noreply.github.com>
2022-08-26 22:43:28 +0600

    Update selectionsort.cpp

--- sort/selectionsort.cpp
+++ sort/selectionsort.cpp
@@ -20,7 +20,7 @@ void selectionSort(int *A, int n){
                 indexOfMin = j;
             }
         }
-        // Swap A[i] and A[indexOfMin]
+        
         temp = A[i];
         A[i] = A[indexOfMin];
         A[indexOfMin] = temp;
@@ -28,25 +28,7 @@ void selectionSort(int *A, int n){
 }
 
 int main(){
-    // Input Array (There will be total n-1 passes. 5-1 = 4 in this case!)
-    //  00  01  02  03  04
-    // |03, 05, 02, 13, 12
-
-    // After first pass
-    //  00  01  02  03  04
-    //  02,|05, 03, 13, 12
-
-    // After second pass
-    // 00  01  02  03  04
-    // 02, 03,|05, 13, 12
-
-    // After third pass
-    // 00  01  02  03  04
-    // 02, 03, 05,|13, 12
-
-    // After fourth pass
-    // 00  01  02  03  04
-    // 02, 03, 05, 12,|13
+    
 
 
     int A[] = {3, 5, 2, 13, 12};
