65a03ceeb84ecf6c7b7a65a882b3bff5cf3157f8
Saima Akter <74361691+Saimatonni@users.noreply.github.com>
2022-08-26 23:00:51 +0600

    Update tree_represent.cpp

--- search_and_tree/tree_represent.cpp
+++ search_and_tree/tree_represent.cpp
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Basic struct of Tree 
+
 struct node 
 { 
     int val; 
     struct node *left, *right; 
 }; 
    
-// Function to create a new Node
+
 struct node* newNode(int item) 
 { 
     struct node* temp =  (struct node *)malloc(sizeof(struct node)); 
@@ -17,7 +17,7 @@ struct node* newNode(int item)
     return temp; 
 } 
    
-// Function print the node in preorder format, when insertion is complete
+
 void preorder(struct node* root) 
 { 
     if (root != NULL) 
@@ -28,23 +28,22 @@ void preorder(struct node* root)
     } 
 } 
    
-// Here we are finding where to insert the new node so BST is followed
+
 struct node* insert(struct node* node, int val) 
 { 
-    /* If the tree(subtree) is empty, return a new node by calling newNode func.*/
+   
     if (node == NULL) return newNode(val); 
-  
-    /* Else, we will do recursion down the tree to further subtrees */
+
     if (val < node->val) 
         node->left  = insert(node->left, val); 
     else if (val > node->val) 
         node->right = insert(node->right, val);    
   
-    /* (Safety) return the node's pointer which is unchanged */
+
     return node; 
 } 
 void display() {  
-    //Node current will point to head 
+    
     struct node *head;
     struct node *current = head;  
     if(head == NULL) {  
@@ -53,7 +52,7 @@ void display() {
     }  
     printf("Nodes of doubly linked list: \n");  
     while(current != NULL) {  
-        //Prints each node by incrementing pointer.  
+       
         printf("%d ", current->val);  
         current = current->right;  
     }  
@@ -61,12 +60,7 @@ void display() {
    
 int main() 
 { 
-    /* Our BST will look like this 
-              200 
-           /     \ 
-          120     280 
-         /  \    /  \ 
-       80   160 240  320 */
+  
     struct node* root = NULL; 
     root = insert(root, 200); 
     insert(root, 120); 
@@ -78,8 +72,7 @@ int main()
     display();
     
    
-    // Finally printing the tree using preorder
-    //preorder(root); 
+   
    
     return 0; 
 }
