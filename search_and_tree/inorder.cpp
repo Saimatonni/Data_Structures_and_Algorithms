5a519723240b836f8ac13d235ad1ca748500e65c
Saima Akter <74361691+Saimatonni@users.noreply.github.com>
2022-08-26 22:55:29 +0600

    Update inorder.cpp

--- search_and_tree/inorder.cpp
+++ search_and_tree/inorder.cpp
@@ -7,9 +7,7 @@ struct Node {
     Node* right;
 };
  
-// Function to create a node with 'value' as the data
-// stored in it.
-// Both the children of this new Node are initially null.
+
 struct Node* newNode(int value)
 {
     Node* n = new Node;
@@ -26,29 +24,22 @@ struct Node* insertValue(struct Node* root, int value,
     if (root == NULL)
         root = node;
  
-    // The left child of the current Node is
-    // used if it is available.
+  
     else if (q.front()->left == NULL)
         q.front()->left = node;
  
-    // The right child of the current Node is used
-    // if it is available. Since the left child of this
-    // node has already been used, the Node is popped
-    // from the queue after using its right child.
+  
     else {
         q.front()->right = node;
         q.pop();
     }
  
-    // Whenever a new Node is added to the tree, its
-    // address is pushed into the queue.
-    // So that its children Nodes can be used later.
+    
     q.push(node);
     return root;
 }
  
-// This function mainly calls insertValue() for
-// all array elements. All calls use same queue.
+
 Node* createTree(int arr[], int n)
 {
     Node* root = NULL;
@@ -58,7 +49,7 @@ Node* createTree(int arr[], int n)
     return root;
 }
  
-// This is used to verify the logic.
+
 void levelOrder(struct Node* root)
 {
     if (root == NULL)
@@ -79,13 +70,13 @@ void printInorder(struct Node* node)
     if (node == NULL)
         return;
  
-    /* first recur on left child */
+   
     printInorder(node->left);
  
-    /* then print the data of node */
+   
     printf("%d ", node->key);
  
-    /* now recur on right child */
+    
     printInorder(node->right);
 }
  
