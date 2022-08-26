9e09a95c4f24912657e3bf3420a83ed6b24cf05b
Saima Akter <74361691+Saimatonni@users.noreply.github.com>
2022-08-26 22:54:28 +0600

    Update expression_tree.cpp

--- search_and_tree/expression_tree.cpp
+++ search_and_tree/expression_tree.cpp
@@ -1,8 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
  
-/* A binary tree node has data, pointer to left child
-   and a pointer to right child */
 struct node {
     char data;
     struct node* left;
@@ -10,8 +8,6 @@ struct node {
     struct node* next;
 };
  struct node *head=NULL;
-/* Helper function that allocates a new node with the
-   given data and NULL left and right pointers. */
 struct node* newNode(char data)
 {
     struct node* node
@@ -28,13 +24,9 @@ void printInorder(struct node* node)
     if (node == NULL)
         return;
     else{
-    /* first recur on left child */
+  
     printInorder(node->left);
- 
-    /* then print the data of node */
     printf("%c ", node->data);
- 
-    /* now recur on right child */
     printInorder(node->right);
     }
 }
@@ -48,32 +40,25 @@ void push(struct node* x)
         (x)->next = head;
         head  = x;
     }
-    // struct node* temp;
-    // while(temp!=NULL)
-    // {
-    //     printf("%c ", temp->data);
-    //     temp = temp->next;
-    // }
+   
 }
 struct node* pop()
 {
-    // Poping out the top most[ pointed with head] element
+    
     struct node* p = head;
     head = head->next;
     return p;
 }
 int main()
 {
-    //char s[] = {'A','B','C','*','+','D','/'};
+   
     string s;
     cin>>s;
-    //int l = sizeof(s) / sizeof(s[0]) ;
+    
     int l = s.length();
     struct node *x, *y, *z;
     for (int i = 0; i < l; i++) {
-        // if read character is operator then poping two
-        // other elements from stack and making a binary
-        // tree
+       
         if (s[i] == '+' || s[i] == '-' || s[i] == '*'
             || s[i] == '/' || s[i] == '^') {
             z = newNode(s[i]);
