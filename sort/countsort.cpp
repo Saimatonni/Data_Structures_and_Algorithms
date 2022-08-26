cc458f232e789563ea3ba773bb7db6313bcde37b
Saima Akter <74361691+Saimatonni@users.noreply.github.com>
2022-08-26 22:42:12 +0600

    Update countsort.cpp

--- sort/countsort.cpp
+++ sort/countsort.cpp
@@ -25,27 +25,26 @@ int maximum(int A[], int n){
 }
 void countSort(int * A, int n){
     int i, j;
-    // Find the maximum element in A
+   
     int max = maximum(A, n); 
 
-    // Create the count array
+   
     int* count = (int *) malloc((max+1)*sizeof(int));
 
-    // Initialize the array elements to 0
+  
     for (i = 0; i < max+1; i++)
     {
         count[i] = 0; 
     }
 
-    // Increment the corresponding index in the count array
+    
     for (i = 0; i < n; i++)
     {
         count[A[i]] = count[A[i]] + 1; 
     }
 
-    i =0; // counter for count array
-    j =0; // counter for given array A
-
+    i =0; 
+    j =0; 
     while(i<= max){
         if(count[i]>0){
             A[j] = i;
