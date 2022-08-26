c1125a27572cbb3a4c3ad00c9dda63e72d638114
Saima Akter <74361691+Saimatonni@users.noreply.github.com>
2022-08-26 22:50:01 +0600

    Update josephus.cpp

--- stack_and_queue/josephus.cpp
+++ stack_and_queue/josephus.cpp
@@ -16,41 +16,36 @@ int Josephus(int n, int k)
     k--;
     int arr[n];
     for (int i = 0; i < n; i++) {
-        arr[i] = 1; // Makes all the 'n' people alive by
-                    // assigning them value = 1
+        arr[i] = 1; 
     }
     int cnt = 0, cut = 0,
-        num = 1; // Cut = 0 gives the sword to 1st person.
+        num = 1; 
     while (
         cnt
-        < (n - 1)) // Loop continues till n-1 person dies.
+        < (n - 1)) 
     {
-        while (num <= k) // Checks next (kth) alive persons.
+        while (num <= k) 
         {
             cut++;
-            cut = cut % n; // Checks and resolves overflow
-                           // of Index.
+            cut = cut % n; 
             if (arr[cut] == 1) {
-                num++; // Updates the number of persons
-                       // alive.
+                num++; 
             }
         }
-        num = 1; // refreshes value to 1 for next use.
+        num = 1; 
         arr[cut]
-            = 0; // Kills the person at position of 'cut'
-        cnt++; // Updates the no. of killed persons.
+            = 0; 
+        cnt++; 
         cut++;
         cut = cut
-              % n; // Checks and resolves overflow of Index.
+              % n; 
         while (arr[cut]
-               == 0) // Checks the next alive person the
-                     // sword is to be given.
+               == 0) 
+                     
         {
             cut++;
-            cut = cut % n; // Checks and resolves overflow
-                           // of Index.
+            cut = cut % n; 
         }
     }
-    return cut + 1; // Output is the position of the last
-                    // man alive(Index + 1);
+    return cut + 1; 
 }
